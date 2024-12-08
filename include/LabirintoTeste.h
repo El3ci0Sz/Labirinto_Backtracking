e47b6074cdb1a357acfb4473b5a0b397cf2fea10
Haunter_kze <rafael.chang@ufv.br>
2024-12-07 21:27:42 -0300

    LabirintoTeste.h

--- include/LabirintoTeste.h
+++ include/LabirintoTeste.h
@@ -1,9 +1,10 @@
 #ifndef LABIRINTO_TESTE_H
 #define LABIRINTO_TESTE_H
 
-#include "Labirinto.h"
+#include "../include/Labirinto.h"
 
 // Função para gerar um labirinto aleatório
 Labirinto* gerar_labirinto_aleatorio(int linhas, int colunas, int num_portas, int num_chaves, int dificuldade);
+
 
 #endif // LABIRINTO_TESTE_H
